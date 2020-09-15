88b5c80bef05f335343f418b66fc881950b25a3e
Anushka-code-pixel11 <71310610+Anushka-code-pixel11@users.noreply.github.com>
2020-09-15 11:51:56 +0530

    Add files via upload

---
+++ collide.js
@@ -0,0 +1,8 @@
+function collide(lbullet,lwall){
+    bulletRightEdge = lbullet.x + lbullet.width;
+    wallLeftEdge = lwall.x;
+    if(bulletRightEdge >= wallLeftEdge){
+      return true;
+    }
+    return false;
+  }
